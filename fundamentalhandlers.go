fiatjaf/websites-for-trello

commit 4f44bd41223d5732a076b46ad964214fb83418f9

deleted attachments should not qualify as valid cover images.

models.go
@@ -3,15 +3,14 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"github.com/jmoiron/sqlx/types"
-	"github.com/shurcooL/go/github_flavored_markdown"
 	"log"
-	"net/http"
-	"net/url"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/jmoiron/sqlx/types"
+	"github.com/shurcooL/go/github_flavored_markdown"
 )
 
 const CARDLINKMATCHEREXPRESSION = "\\]\\(https?://trello.com/c/([^/]+)(/[\\w-]*)?\\)"
@@ -265,7 +264,14 @@ func (card Card) HasCover() bool {
 	if card.Cover == "" {
 		return false
 	}
-	return true
+	/* cover must be in the attachments array */
+	for _, attachment := range card.GetAttachments() {
+		if attachment.Url == card.Cover {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (card Card) GetChecklists() []Checklist {

commit 52280a17c3076083c3609e22e1d6ff70e57cf0b9

fix error introduced by 4f44bd41223d5732a076b46ad964214fb83418f9.
attachments must be fetched so they are compared against the cover
URL.

this is inneficient. something must be done about it in the future.
perhaps postgres can do this COVER IN ATTACHMENTS check instead of
our server, or we could do better cover image x attachment handling
so the check isn't needed.

fundamentalhandlers.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/MindscapeHQ/raygun4go"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/MindscapeHQ/raygun4go"
+	"github.com/gorilla/mux"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -96,7 +97,8 @@ func label(w http.ResponseWriter, r *http.Request) {
          id,
          '""'::json AS users,
          0 AS pos,
-         '' AS cover
+         '' AS cover,
+         '""'::jsonb AS attachments
   FROM labels
   WHERE board_id = $2
     AND slug = $3
@@ -109,7 +111,8 @@ func label(w http.ResponseWriter, r *http.Request) {
          cards.id,
          array_to_json(array(SELECT row_to_json(u) FROM users AS u WHERE u._id = ANY(cards.users))) AS users,
          cards.pos,
-         coalesce(cards.cover, '') AS cover
+         coalesce(cards.cover, '') AS cover,
+         cards.attachments->'attachments' AS attachments
   FROM cards
   INNER JOIN labels
   ON labels.id = ANY(cards.labels)
@@ -212,8 +215,8 @@ FROM (
            cards.due,
            cards.id,
            cards.desc,
-           cards.attachments->'attachments',
-           cards.checklists->'checklists',
+           cards.attachments->'attachments' AS attachments,
+           cards.checklists->'checklists' AS checklists,
            array_to_json(array(SELECT row_to_json(l) FROM labels AS l WHERE l.id = ANY(cards.labels) AND l.visible)) AS labels,
            array_to_json(array(SELECT row_to_json(u) FROM users AS u WHERE u._id = ANY(cards.users))) AS users,
            1 AS sort,
@@ -323,7 +326,8 @@ func list(w http.ResponseWriter, r *http.Request) {
          '""'::json AS labels,
          '""'::json AS users,
          0 AS pos,
-         '' AS cover
+         '' AS cover,
+         '""'::jsonb AS attachments
   FROM lists
   WHERE board_id = $2
     AND slug = $3
@@ -337,7 +341,8 @@ func list(w http.ResponseWriter, r *http.Request) {
          array_to_json(array(SELECT row_to_json(l) FROM labels AS l WHERE l.id = ANY(cards.labels) AND l.visible)) AS labels,
          array_to_json(array(SELECT row_to_json(u) FROM users AS u WHERE u._id = ANY(cards.users))) AS users,
          cards.pos,
-         coalesce(cards.cover, '') AS cover
+         coalesce(cards.cover, '') AS cover,
+         cards.attachments->'attachments' AS attachments
   FROM cards
   INNER JOIN lists
   ON lists.id = cards.list_id

helpers.go
@@ -238,11 +238,12 @@ func completeWithIndexCards(requestData *RequestData) error {
 SELECT cards.slug,
        cards.name,
        substring(cards.desc from 0 for $1) AS excerpt,
-       coalesce(cards.cover, '') AS cover,
        array_to_string(array_prepend('https://trello.com/c/' || cards.id, syndicated), $5) AS syndicated,
        cards.id,
        CASE WHEN due IS NOT NULL THEN due ELSE (to_timestamp(hex_to_int(left(cards.id, 8)))) END AS due,
-       list_id
+       list_id,
+       coalesce(cards.cover, '') AS cover,
+       cards.attachments->'attachments' AS attachments
 FROM cards
 INNER JOIN lists ON lists.id = cards.list_id
 WHERE lists.board_id = $2

models.go
@@ -126,13 +126,12 @@ func (card Card) HasExcerpt() bool {
 }
 
 func (card Card) HasCover() bool {
-	if card.Cover == "" {
-		return false
-	}
-	/* cover must be in the attachments array */
-	for _, attachment := range card.GetAttachments() {
-		if attachment.Url == card.Cover {
-			return true
+	if card.Cover != "" {
+		/* cover must be in the attachments array */
+		for _, attachment := range card.GetAttachments() {
+			if attachment.Url == card.Cover {
+				return true
+			}
 		}
 	}
 